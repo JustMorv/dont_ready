b53d3dc06b2af4db762dda647284064f2371e368
JustMorv <89212312+JustMorv@users.noreply.github.com>
2023-12-04 12:10:04 +0600

    fff

---
+++ db/db.js
@@ -0,0 +1,13 @@
+let productCard = [
+    {id: '001',img:"img/product/1.webp", title:"какая-то карточка", deck:"Lorem ipsum dolor sit amet consectetur adipisicing elit.", price: "1500₸"},
+    {id: '002',img:"img/product/1.webp", title:"какая-то карточка", deck:"Lorem ipsum dolor sit amet consectetur adipisicing elit.", price: "1500₸"},
+    {id: '003',img:"img/product/1.webp", title:"какая-то карточка", deck:"Lorem ipsum dolor sit amet consectetur adipisicing elit.", price: "1500₸"},
+    {id: '004',img:"img/product/1.webp", title:"какая-то карточка", deck:"Lorem ipsum dolor sit amet consectetur adipisicing elit.", price: "1500₸"},
+    {id: '005',img:"img/product/1.webp", title:"какая-то карточка", deck:"Lorem ipsum dolor sit amet consectetur adipisicing elit.", price: "1500₸"},
+    {id: '006',img:"img/product/1.webp", title:"какая-то карточка", deck:"Lorem ipsum dolor sit amet consectetur adipisicing elit.", price: "1500₸"},
+    {id: '007',img:"img/product/1.webp", title:"какая-то карточка", deck:"Lorem ipsum dolor sit amet consectetur adipisicing elit.", price: "1500₸"},
+    {id: '008',img:"img/product/1.webp", title:"какая-то карточка", deck:"Lorem ipsum dolor sit amet consectetur adipisicing elit.", price: "1500₸"},
+    {id: '009',img:"img/product/1.webp", title:"какая-то карточка", deck:"Lorem ipsum dolor sit amet consectetur adipisicing elit.", price: "1500₸"},
+    {id: '010',img:"img/product/1.webp", title:"какая-то карточка", deck:"Lorem ipsum dolor sit amet consectetur adipisicing elit.", price: "1500₸"},
+
+]
